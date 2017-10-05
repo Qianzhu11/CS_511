21c6c6d411484e142ae4834b4d3c5f1599bc35ae
Qianzhu11 <2858583565@qq.com>
2017-10-05 17:04:43 -0400

    Generating random apparatus type

--- ApparatusType.java
+++ ApparatusType.java
@@ -1,7 +1,25 @@
 package Assignment2;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ApparatusType {
+	
+	private at apparatusType;
+	
+	public ApparatusType(at apparatusType) {
+		this.apparatusType = apparatusType;
+	}
+	
+	public enum at {
+		LEGPRESSMACHINE, BARBELL, HACKSQUATMACHINE, LEGEXTENSIONMACHINE, LEGCURLMACHINE, 
+		LATPULLDOWNMACHINE , PECDECKMACHINE , CABLECROSSOVERMACHINE
+	}
+	
 	public static ApparatusType generateRandom(int id) {
-		return null;
+		List<at> ApparatusTypeToList = Arrays.asList(at.values());
+		int atLength = ApparatusTypeToList.size();
+		int i = (int)Math.random() * atLength;
+		return new ApparatusType(ApparatusTypeToList.get(i));
 	}
 }
